91d8109788716d4b143c33ad0241645eff74e8ec
SebFlatBeat <sebastien.darre@gmail.com>
2019-04-03 22:07:28 +0200

    Ajout de librairies, implementation de la classe abstraite Game et utilisation des surcharges de methode dans la classe Recherche

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,6 +1,214 @@
-public abstract class Game {
-protected String challengerMode;
-protected String defenseMode;
-protected String duelMode;
+import org.apache.commons.text.RandomStringGenerator;
 
+import java.util.Scanner;
+
+
+public abstract class Game {
+    static Scanner entree = new Scanner(System.in);
+    protected int nbMaxEssais;
+    protected int nbChiffreCpu;
+    protected int nbChiffreJoueur;
+    protected int nombreEssais;
+    protected int essaiRestant;
+    protected boolean sortir;
+    protected String JoueurNbMystere;
+    protected String[] JoueurTabMystere;
+    protected String CpuNbMystere;
+    protected String[] CpuTabMystere;
+    protected String premiereProposition;
+    protected String[] premierePropositionTab;
+    protected RandomStringGenerator generator = new RandomStringGenerator.Builder()
+            .withinRange('0','9').build();
+
+    public String getRandomNumber() {
+        return randomNumber;
+    }
+
+    public void setRandomNumber(String randomNumber) {
+        this.randomNumber = randomNumber;
+    }
+
+    protected String randomNumber = generator.generate(4,10);
+
+    public String getPremiereProposition() {
+        return premiereProposition;
+    }
+
+    public void setPremiereProposition(String premiereProposition) {
+        this.premiereProposition = premiereProposition;
+    }
+
+    public String[] getPremierePropositionTab() {
+        return premierePropositionTab;
+    }
+
+    public void setPremierePropositionTab(String[] premierePropositionTab) {
+        this.premierePropositionTab = premierePropositionTab;
+    }
+
+    public int getNombreEssais() {
+        return nombreEssais;
+    }
+
+    public void setNombreEssais(int nombreEssais) {
+        this.nombreEssais = nombreEssais;
+    }
+
+    public int getEssaiRestant() {
+        return essaiRestant;
+    }
+
+    public void setEssaiRestant(int essaiRestant) {
+        this.essaiRestant = essaiRestant;
+    }
+
+    public int getNbChiffreCpu() {
+        return nbChiffreCpu;
+    }
+
+    public void setNbChiffreCpu(int nbChiffreCpu) {
+        this.nbChiffreCpu = nbChiffreCpu;
+    }
+
+    public String getJoueurNbMystere() {
+        return JoueurNbMystere;
+    }
+
+    public void setJoueurNbMystere(String joueurNbMystere) {
+        JoueurNbMystere = joueurNbMystere;
+    }
+
+    public String[] getJoueurTabMystere() {
+        return JoueurTabMystere;
+    }
+
+    public void setJoueurTabMystere(String[] joueurTabMystere) {
+        JoueurTabMystere = joueurTabMystere;
+    }
+
+    public String getCpuNbMystere() {
+        return CpuNbMystere;
+    }
+
+    public void setCpuNbMystere(String cpuNbMystere) {
+        CpuNbMystere = cpuNbMystere;
+    }
+
+    public String[] getCpuTabMystere() {
+        return CpuTabMystere;
+    }
+
+    public void setCpuTabMystere(String[] cpuTabMystere) {
+        CpuTabMystere = cpuTabMystere;
+    }
+
+    public int getNbMaxEssais() {
+        return nbMaxEssais;
+    }
+
+
+    public void setNbMaxEssais(int nbMaxEssais) {
+        this.nbMaxEssais = nbMaxEssais;
+
+    }
+
+    public int getNbChiffreJoueur() {
+        return nbChiffreJoueur;
+    }
+
+    public void setNbChiffreJoueur(int nbChiffreJoueur) {
+        this.nbChiffreJoueur = nbChiffreJoueur;
+    }
+
+    protected void challengerMode () {
+        setNbMaxEssais(10);
+        setNbChiffreCpu(0);
+        setNombreEssais(1);
+        setEssaiRestant(getNbMaxEssais());
+        setCpuNbMystere(randomNumber);
+        setCpuTabMystere(CpuNbMystere.split("(?<=.)"));
+        for (int i = 0; i < CpuTabMystere.length; i++) {
+            nbChiffreCpu++;
+        }
+    }
+
+    protected void defenseMode () {
+        setNbMaxEssais(10);
+        setNbChiffreJoueur(0);
+        setNombreEssais(1);
+        setEssaiRestant(getNbMaxEssais());
+        setPremiereProposition("");
+        do {
+            System.out.println("Vous pouvez entrer une combinaision de 1 à 10 chiffres");
+            System.out.println("Entrez votre combinaison secrète : "); //Faire une limitation de l'intervalle
+            setJoueurNbMystere(entree.next());
+            while (getJoueurNbMystere().matches("^[a-zA-Z]*$")) {
+                System.out.println("Vous ne devez mettre que des chiffres");
+                System.out.println("Vous pouvez entrer une combinaision de 1 à 10 chiffres");
+                setJoueurNbMystere(entree.next());
+            }
+            setJoueurTabMystere(JoueurNbMystere.split("(?<=.)"));
+            /**
+             * Décompte de nbChiffre pour donner un indice au joueur
+             */
+            for (int i = 0; i< JoueurTabMystere.length; i++){
+                nbChiffreJoueur++;
+            }
+            if (nbChiffreJoueur<1 || nbChiffreJoueur>10) {
+                System.out.println("Vous devez entrer une combinaision de 1 à 10 chiffres");
+                sortir = false;
+            }else{
+                sortir = true;
+            }
+        }while (!sortir);
+        setPremierePropositionTab(new String[getNbChiffreJoueur()]);
+
+    }
+
+
+    protected void duelMode () {
+        setNbMaxEssais(10);
+        setNbChiffreCpu(0);
+        setNbChiffreJoueur(0);
+        /**
+         * CPU
+         */
+
+        setCpuNbMystere(randomNumber);
+        setCpuTabMystere(CpuNbMystere.split("(?<=.)"));
+        for (int i = 0; i < CpuTabMystere.length; i++) {
+            nbChiffreCpu++;
+        }
+        /**
+         * player
+         */
+        do {
+            System.out.println("Vous pouvez entrer une combinaision de 1 à 10 chiffres");
+            System.out.println("Entrez votre combinaison secrète : "); //Faire une limitation de l'intervalle
+            JoueurNbMystere = entree.next();
+            while (JoueurNbMystere.matches("^[a-zA-Z]*$")) {
+                System.out.println("Vous ne devez mettre que des chiffres");
+                System.out.println("Vous pouvez entrer une combinaision de 1 à 10 chiffres");
+                JoueurNbMystere = entree.next();
+            }
+            JoueurTabMystere = JoueurNbMystere.split("(?<=.)");
+            /**
+             * Décompte de nbChiffre pour donner un indice au joueur
+             */
+            for (int i = 0; i< JoueurTabMystere.length; i++){
+                nbChiffreJoueur++;
+            }
+            if (nbChiffreJoueur<1 || nbChiffreJoueur>10) {
+                System.out.println("Vous devez entrer une combinaision de 1 à 10 chiffres");
+                sortir = false;
+            }else{
+                sortir = true;
+            }
+        }while (!sortir);
+    }
+    protected void developperMode () {
+
+    }
 }
+
+
--- src/main/java/Recherche.java
+++ src/main/java/Recherche.java
@@ -1,115 +1,34 @@
-import java.util.InputMismatchException;
-import java.util.Scanner;
 import java.util.Arrays;
+import java.util.Scanner;
 
-public class Recherche  {
+public class Recherche  extends Game {
 
     static Scanner entree = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        /**
-         * Saisi du nombre maximum d'essai en limitant le choix et en ajoutant une exception
-         */
-        int nbMaxEssais;
-        boolean sortir;
-        do {
-            nbMaxEssais = 0;
-            System.out.println("Combien d'essais sont autorisés ? Choississez un nombre entre 3 et 20 ");
-            try {
-                sortir = true;
-                nbMaxEssais = entree.nextInt();
-            } catch (InputMismatchException e) {
-                System.out.println("Vous ne devez mettre que des chiffres");
-                entree.next();
-                sortir = false;
-            }
-            if (nbMaxEssais<3 || nbMaxEssais>20 && sortir) {
-                System.out.println("Vous devez choisir un nombre entre 3 et 20 !!!!");
-                sortir = false;
-            }
-        }while (!sortir);
-
-
-        /**
-         * Saisi du chiffre mystere et passage en format tableau
-         */
-        String nbMystere;
-        String[] tabMystere;
-        int nbChiffre = 0;
-        do {
-           System.out.println("Vous pouvez entrer une combinaision de 1 à 10 chiffres");
-           System.out.println("Entrez votre combinaison secrète : "); //Faire une limitation de l'intervalle
-               nbMystere = entree.next();
-               while (nbMystere.matches("^[a-zA-Z]*$")) {
-                   System.out.println("Vous ne devez mettre que des chiffres");
-                   System.out.println("Vous pouvez entrer une combinaision de 1 à 10 chiffres");
-                   nbMystere = entree.next();
-                   }
-           tabMystere = nbMystere.split("(?<=.)");
-            /**
-             * Décompte de nbChiffre pour donner un indice au joueur
-             */
-            for (int i = 0; i< tabMystere.length; i++){
-                nbChiffre = nbChiffre+1;
-            }
-            if (nbChiffre<1 || nbChiffre>10) {
-                System.out.println("Vous devez entrer une combinaision de 1 à 10 chiffres");
-                sortir = false;
-            }else{
-                sortir = true;
-            }
-       }while (!sortir);
-
-
-
-
-        System.out.println(" ");
-        System.out.println(" ");
-        System.out.println(" ");
+    @Override
+    public void challengerMode() {
+        super.challengerMode();
         System.out.println("Bienvenue sur le jeu : Recherche +/-");
         System.out.println("Tu dois trouver la combinaison secrète. A chaque proposition des indices te seront donnés");
         System.out.println("les indices sont : '+' '-' et '=' ");
-        System.out.println("Tu as le droit à " +nbMaxEssais+" essais pour y arriver !");
-        System.out.println("Pour t'aider sache que la combinaison secrète est à "+nbChiffre+" chiffres.");
-        System.out.println(" ");
-        System.out.println(" ");
-        System.out.println(" ");
+        System.out.println("Tu as le droit à " + getNbMaxEssais() + " essais pour y arriver !");
+        System.out.println("Pour t'aider sache que la combinaison secrète est à " + getNbChiffreCpu() + " chiffres.");
 
-
-        /**
-         * Intialisation de la variable nbReponse
-         * Comparaison de la taille des deux tableaux tabReponse et tabMystere
-         */
         System.out.println("Entrez votre proposition : "); //Pense à catcher une exception afin que nbReponse ne soit pas inferieur ou superieur nbChiffre
         String nbReponse = entree.next();
         String[] tabReponse = nbReponse.split("(?<=.)");
-        while (tabReponse.length != tabMystere.length){
+
+        while (tabReponse.length != getCpuTabMystere().length) {
             System.out.println("Votre réponse ne fait pas la meme taille que la combinaison secrète !!");
-            System.out.println("Veuillez rentrer une reponse qui fait la taille de "+nbChiffre+" chiffre(s)");
+            System.out.println("Veuillez rentrer une reponse qui fait la taille de " + nbChiffreCpu + " chiffre(s)");
             nbReponse = entree.next();
             tabReponse = nbReponse.split("(?<=.)");
         }
 
-        /**
-         * Intialisation de la variable nombreEssais à 1 puisque le joueur démarre directement avec son premier essai
-         */
-        int nombreEssais = 1;
-
-        /**
-         * Initialisation de la variable essaiRestant
-         */
-        int essaiRestant = nbMaxEssais;
-
-        /**
-         * Boucle permettant d'indiquer si c'est plus ou moins ou égal en comparant l'égalité de tabReponse et tabMystere tant que nombreEssais est plus petit que nbMaxEssais
-         * Décrémentation de essaiRestant
-         * Incrémentation de nombreEssais
-         * */
-
-        while (!Arrays.equals(tabReponse, tabMystere) && nombreEssais < nbMaxEssais) {
-            String[] compare = new String[tabMystere.length];
-            for (int i = 0; i < tabMystere.length; i++) {
-                int valeur = tabMystere[i].compareTo(tabReponse[i]);
+        while (!Arrays.equals(tabReponse, getCpuTabMystere()) && getNombreEssais() < getNbMaxEssais()) {
+            String[] compare = new String[getCpuTabMystere().length];
+            for (int i = 0; i < getCpuTabMystere().length; i++) {
+                int valeur = getCpuTabMystere()[i].compareTo(tabReponse[i]);
                 if (valeur == 0) {
                     compare[i] = "=";
                 } else if (valeur > 0) {
@@ -118,29 +37,24 @@ public class Recherche  {
                     compare[i] = "-";
                 }
             }
-            essaiRestant = essaiRestant-1;
+            essaiRestant = essaiRestant - 1;
             System.out.println("Vous avez proposé : " + nbReponse + " -> Voici des indices pour vous aider : " + Arrays.toString(compare));
-            System.out.println("Il te reste "+essaiRestant+ " essais !");
+            System.out.println("Il te reste " + essaiRestant + " essais !");
             System.out.println("Entrez votre nouvelle proposition : ");
             nbReponse = entree.next();
             tabReponse = nbReponse.split("(?<=.)");
-            while (tabReponse.length != tabMystere.length){
+            while (tabReponse.length != getCpuTabMystere().length) {
                 System.out.println("Votre réponse ne fait pas la meme taille que la combinaison secrète !!");
-                System.out.println("Veuillez rentrer une reponse qui fait la taille de "+nbChiffre);
+                System.out.println("Veuillez rentrer une reponse qui fait la taille de " + nbChiffreCpu);
                 nbReponse = entree.next();
                 tabReponse = nbReponse.split("(?<=.)");
             }
             nombreEssais = nombreEssais + 1;
-
         }
-        /**
-         * Si nombreEssais est plus grand ou égal à nbMaxEssais et que tabReponse et tabMystere ne sont pas égaux fin de partie
-         * Sinon afficher Félicitations
-         */
-        if (nombreEssais >= nbMaxEssais && !Arrays.equals(tabReponse, tabMystere)) {
-            String[] compare = new String[tabMystere.length];
-            for (int i = 0; i < tabMystere.length; i++) {
-                int valeur = tabMystere[i].compareTo(tabReponse[i]);
+        if (nombreEssais >= getNbMaxEssais() && !Arrays.equals(tabReponse, getCpuTabMystere())) {
+            String[] compare = new String[getCpuTabMystere().length];
+            for (int i = 0; i < getCpuTabMystere().length; i++) {
+                int valeur = getCpuTabMystere()[i].compareTo(tabReponse[i]);
                 if (valeur == 0) {
                     compare[i] = "=";
                 } else if (valeur > 0) {
@@ -150,13 +64,56 @@ public class Recherche  {
                 }
             }
             System.out.println("Vous avez proposé : " + nbReponse + " -> Voici des indices pour vous aider : " + Arrays.toString(compare));
-            System.out.println("Tu as dépassé le nombre d'essais autorisé qui était de " + nbMaxEssais+ "  essai(s)");
-            System.out.println("Voici la solution : "+ tabReponse);
-        }else{
+            System.out.println("Mais tu as dépassé le nombre d'essais autorisé qui était de " + getNbMaxEssais() + "  essai(s)");
+            System.out.println("Voici la solution : " + getCpuNbMystere());
+        } else {
             System.out.println("Félicitations !!! Tu as reussi avec " + nombreEssais + " essai(s)");
+        }
+    }
+
+
+    @Override
+    public void defenseMode() {
+        super.defenseMode();
+        String nbReponse;
+        System.out.println("Bienvenue sur le jeu : Recherche +/-");
+        System.out.println("L'ordinateur doit trouver ta combinaison secrète.");
+        System.out.println("Il a le droit à " + getNbMaxEssais() + " essais pour y arriver !");
+        System.out.println("Ta combinaison secrète est à " + getNbChiffreJoueur() + " chiffres.");
+
+        System.out.println("L'ordinateur entre sa proposition : ");
+        for (int i = 0; i < getNbChiffreJoueur(); i++) {
+            getPremierePropositionTab()[i] = String.valueOf(5);
+            premiereProposition = getPremiereProposition() + getPremierePropositionTab()[i];
+        }
+        nbReponse = premiereProposition;
+        String[] tabReponse = getPremiereProposition().split("(?<=.)");
+        while (!Arrays.equals(tabReponse, getJoueurTabMystere()) && getNombreEssais() < getNbMaxEssais()) {
+            String[] compare = new String[getJoueurTabMystere().length];
+            for (int i = 0; i < getJoueurTabMystere().length; i++) {
+                int valeur = getJoueurTabMystere()[i].compareTo(tabReponse[i]);
+                if (valeur == 0) {
+                    compare[i] = "=";
+                } else if (valeur > 0) {
+                    compare[i] = "+";
+                } else if (valeur < 0) {
+                    compare[i] = "-";
+                }
+            }
+            essaiRestant = essaiRestant - 1;
+            nombreEssais = nombreEssais + 1;
+            System.out.println("Vous avez proposé : " + nbReponse + " -> Voici des indices pour vous aider : " + Arrays.toString(compare));
+            System.out.println("Il te reste " + essaiRestant + " essais !");
+            System.out.println("Entrez votre nouvelle proposition : ");
         }
 
     }
+
+        @Override
+        protected void duelMode() {
+            super.duelMode();
+        }
 }
+
 
 
